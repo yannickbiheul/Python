6587107653f35e02d02095f2132360b9cd2257a6
Yannick Biheul <yannickbiheul@outlook.fr>
2024-03-19 16:18:18 +0100

    Boucle while

--- Programme_1/main.py
+++ Programme_1/main.py
@@ -2,16 +2,23 @@
 Premier programme
 """
 
-# On demande le prénom et l'âge
-prenom = input("Quel est ton prénom ?\n")
-age = input("Quel est ton âge ?\n")
-try:
-    ageProchain = int(age) + 1
-    # On affiche les résultats
-except ValueError:
-    print("ERREUR : Tu dois entrer un nombre !")
-else:
-    print("Tu t'appelle " + prenom + " et tu as " + age + " ans.")
-    print("L'an prochain, tu auras " + str(ageProchain) + " ans.")
+# On demande le prénom
+prenom = ""
+while prenom == "":
+    prenom = input("Quel est ton prénom ?\n")
+
+# On demande l'âge
+age = 0
+while age == 0:
+    age_str = input("Quel est ton âge ?\n")
+    try:
+        age = int(age_str)
+    except:
+        print("ERREUR : Tu dois entrer un nombre !")
+        
+# On affiche les résultats
+print("Tu t'appelle " + prenom + " et tu as " + str(age) + " ans.")
+print("L'an prochain, tu auras " + str(age+1) + " ans.")
+
 
 
