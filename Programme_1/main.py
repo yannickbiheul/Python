af4c2e37e30390b8cfdb083dec22602ed02dc282
Yannick Biheul <yannickbiheul@outlook.fr>
2024-03-19 16:45:17 +0100

    Fonctions

--- Programme_1/main.py
+++ Programme_1/main.py
@@ -2,19 +2,31 @@
 Premier programme
 """
 
+# Fonction pour demander le prénom
+def demander_prenom():
+    reponse_prenom = ""
+    while reponse_prenom == "":
+        reponse_prenom = input("Quel est ton prénom ?\n")
+    return reponse_prenom
+
+
+# Fonction pour demander l'âge
+def demander_age():
+    age_int = 0
+    while age_int == 0:
+        age_str = input("Quel est ton âge ?\n")
+        try:
+            age_int = int(age_str)
+        except:
+            print("ERREUR : Tu dois entrer un nombre !")
+    return age_int
+
+
 # On demande le prénom
-prenom = ""
-while prenom == "":
-    prenom = input("Quel est ton prénom ?\n")
+prenom = demander_prenom()
 
 # On demande l'âge
-age = 0
-while age == 0:
-    age_str = input("Quel est ton âge ?\n")
-    try:
-        age = int(age_str)
-    except:
-        print("ERREUR : Tu dois entrer un nombre !")
+age = demander_age()
         
 # On affiche les résultats
 print("Tu t'appelle " + prenom + " et tu as " + str(age) + " ans.")
